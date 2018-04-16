b1ee7f5cfea69b1bdb4d790675c7c351f9679a71
Sean Rhone <Espionage724@users.noreply.github.com>
2018-04-16 01:41:59 -0400

    Update Maintenance.bat

--- Maintenance.bat
+++ Maintenance.bat
@@ -1,4 +1,9 @@
 "%windir%\system32\Dism.exe" /online /Cleanup-Image /StartComponentCleanup /ResetBase /RestoreHealth
 "%windir%\system32\sfc.exe" /scannow
-"%windir%\system32\cleanmgr.exe"
+"%windir%\system32\cleanmgr.exe" /sageset:65535
+"%windir%\system32\cleanmgr.exe" /sagerun:65535
+rmdir "%temp%" /S /Q
+mkdir "%temp%"
+rmdir "%windir%\temp" /S /Q
+mkdir "%windir%\temp"
 "%windir%\system32\dfrgui.exe"
