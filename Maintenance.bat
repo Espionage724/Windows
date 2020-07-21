5bc1008b91136215a0c7fb8f816fbaf8ac7f64d8
Sean Rhone <Espionage724@users.noreply.github.com>
2020-07-21 21:45:16 +0000

    Update Maintenance.bat
    - Remove temp folder clean-up
    - Add .NET optimization commands

--- Maintenance.bat
+++ Maintenance.bat
@@ -1,9 +1,12 @@
+"%windir%\microsoft.net\Framework\v4.0.30319\ngen.exe" update /force /queue
+"%windir%\microsoft.net\Framework64\v4.0.30319\ngen.exe" update /force /queue
+"%windir%\microsoft.net\Framework\v4.0.30319\ngen.exe" executequeueditems
+"%windir%\microsoft.net\Framework64\v4.0.30319\ngen.exe" executequeueditems
+
 "%windir%\system32\Dism.exe" /online /Cleanup-Image /StartComponentCleanup /ResetBase /RestoreHealth
 "%windir%\system32\sfc.exe" /scannow
+
 "%windir%\system32\cleanmgr.exe" /sageset:65535
 "%windir%\system32\cleanmgr.exe" /sagerun:65535
-rmdir "%temp%" /S /Q
-mkdir "%temp%"
-rmdir "%windir%\temp" /S /Q
-mkdir "%windir%\temp"
+
 "%windir%\system32\dfrgui.exe"
