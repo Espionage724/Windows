416aeaaae4e19882afa7fdeb9817cfdb2d82ca1d
Sean Rhone <Espionage724@users.noreply.github.com>
2024-05-09 17:33:40 -0400

    Add files via upload

---
+++ Compat Flags/Old School RuneScape (Legacy Java).bat
@@ -0,0 +1,2 @@
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "%UserProfile%\jagexcache\jagexlauncher\bin\JagexLauncher.exe" /t "REG_SZ" /d "~ DISABLEDXMAXIMIZEDWINDOWEDMODE HIGHDPIAWARE DISABLEDWM DISABLETHEMES" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\JagexLauncher.exe" /v "UseLargePages" /t "REG_DWORD" /d "1" /f
