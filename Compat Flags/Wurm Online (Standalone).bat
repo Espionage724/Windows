3241c9d87f3f56cddeeb22e4258fad3dfc39239b
Sean Rhone <Espionage724@users.noreply.github.com>
2024-05-17 17:19:32 -0400

    Add files via upload

---
+++ Compat Flags/Wurm Online (Standalone).bat
@@ -0,0 +1,2 @@
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "%localappdata%\Programs\Wurm Online\WurmLauncher64.exe" /t "REG_SZ" /d "~ DISABLEDXMAXIMIZEDWINDOWEDMODE HIGHDPIAWARE DISABLEDWM DISABLETHEMES" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\WurmLauncher64.exe" /v "UseLargePages" /t "REG_DWORD" /d "1" /f
