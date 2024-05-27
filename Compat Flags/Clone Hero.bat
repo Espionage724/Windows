e95126c2924a1aeefdeafb85908e126d8adcb5bb
Sean Rhone <Espionage724@users.noreply.github.com>
2024-05-26 22:54:05 -0400

    Add files via upload

---
+++ Compat Flags/Clone Hero.bat
@@ -0,0 +1,2 @@
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files\Clone Hero\Clone Hero.exe" /t "REG_SZ" /d "~ DISABLEDXMAXIMIZEDWINDOWEDMODE HIGHDPIAWARE DISABLEDWM DISABLETHEMES" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Clone Hero.exe" /v "UseLargePages" /t "REG_DWORD" /d "1" /f
