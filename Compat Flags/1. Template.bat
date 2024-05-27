4e23e179fbefb750222697165a674ba879e948cf
Sean Rhone <Espionage724@users.noreply.github.com>
2024-05-26 23:00:40 -0400

    Update 1. Template.bat

--- Compat Flags/1. Template.bat
+++ Compat Flags/1. Template.bat
@@ -1,2 +1,4 @@
+:: Remove ::'s (below), change executable names, and paths for AppCompatFlags\Layers
+
 ::reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files\devilutionx\devilutionx.exe" /t "REG_SZ" /d "~ DISABLEDXMAXIMIZEDWINDOWEDMODE HIGHDPIAWARE DISABLEDWM DISABLETHEMES" /f
 ::reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\devilutionx.exe" /v "UseLargePages" /t "REG_DWORD" /d "1" /f
