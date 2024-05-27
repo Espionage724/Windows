47b8138273709c101202489df2cb6c978250613e
Sean Rhone <Espionage724@users.noreply.github.com>
2024-05-26 22:57:51 -0400

    Add files via upload

---
+++ Compat Flags/Unreal Tournament 99 (Steam).bat
@@ -0,0 +1,2 @@
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files (x86)\Steam\steamapps\common\Unreal Tournament\System\UnrealTournament.exe" /t "REG_SZ" /d "~ DISABLEDXMAXIMIZEDWINDOWEDMODE HIGHDPIAWARE DISABLEDWM DISABLETHEMES" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\UnrealTournament.exe" /v "UseLargePages" /t "REG_DWORD" /d "1" /f
