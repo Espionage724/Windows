ea3f235a495373aeedc6bc578d3f19a699fe5d75
Sean Rhone <Espionage724@users.noreply.github.com>
2024-05-26 22:42:53 -0400

    Update World of Warcraft (3.3.5).bat

--- Compat Flags/World of Warcraft (3.3.5).bat
+++ Compat Flags/World of Warcraft (3.3.5).bat
@@ -1,2 +1,4 @@
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files (x86)\World of Warcraft 3.3.5\Wow.exe" /t "REG_SZ" /d "~ DISABLEDXMAXIMIZEDWINDOWEDMODE HIGHDPIAWARE DISABLEDWM DISABLETHEMES" /f
+
+:: WARNING: Retail WoW uses the same Wow.exe exectuable name and is not compatible with UseLargePages
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Wow.exe" /v "UseLargePages" /t "REG_DWORD" /d "1" /f
