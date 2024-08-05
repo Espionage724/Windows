ead73d2461edefb78c252740e84fed9506d16b97
Sean Rhone <Espionage724@users.noreply.github.com>
2024-08-05 14:50:50 -0400

    Add files via upload

---
+++ Compat Flags/Lord of the Rings Online (Steam).bat
@@ -0,0 +1,2 @@
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files (x86)\Steam\steamapps\common\Lord of the Rings Online\x64\lotroclient64.exe" /t "REG_SZ" /d "~ DISABLEDXMAXIMIZEDWINDOWEDMODE HIGHDPIAWARE DISABLEDWM DISABLETHEMES" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\lotroclient64.exe" /v "UseLargePages" /t "REG_DWORD" /d "1" /f
---
+++ Compat Flags/Titan Quest AE (Steam).bat
@@ -0,0 +1,2 @@
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files (x86)\Steam\steamapps\common\Titan Quest Anniversary Edition\TQ.exe" /t "REG_SZ" /d "~ DISABLEDXMAXIMIZEDWINDOWEDMODE HIGHDPIAWARE DISABLEDWM DISABLETHEMES" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\TQ.exe" /v "UseLargePages" /t "REG_DWORD" /d "1" /f
