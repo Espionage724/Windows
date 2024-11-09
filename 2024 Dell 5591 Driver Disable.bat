8e992b08d89359f8303a10e3a07513dabb7b1130
Sean Rhone <Espionage724@users.noreply.github.com>
2024-11-08 22:36:08 -0500

    Update 2024 Dell 5591 Driver Disable.bat
    Only the touchpad driver is annoying

--- 2024 Dell 5591 Driver Disable.bat
+++ 2024 Dell 5591 Driver Disable.bat
@@ -1,5 +1,5 @@
 :: Dell Latitude 5591
-:: 2024/04/27
+:: 2024/05/19
 
 :: Initial
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions" /v "DenyDeviceIDs" /t "REG_DWORD" /d "1" /f
@@ -15,38 +15,4 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\Den
 sc stop "ApHidMonitorService"
 sc config "ApHidMonitorService" start="disabled"
 
-:: Intel(R) Dynamic Tuning Manager
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "3" /t "REG_SZ" /d "ACPI\VEN_INT&DEV_3400" /f
-::reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "3" /f
-sc stop "esifsvc"
-sc config "esifsvc" start="disabled"
-
-:: Intel(R) Dynamic Tuning Processor Participant
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "4" /t "REG_SZ" /d "PCI\VEN_8086&DEV_1903&SUBSYS_08191028&REV_07" /f
-::reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "4" /f
-
-:: Intel(R) Dynamic Tuning Generic Participant
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "5" /t "REG_SZ" /d "ACPI\VEN_INT&DEV_3403" /f
-::reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "5" /f
-
-:: Intel(R) HID Event Filter
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "6" /t "REG_SZ" /d "ACPI\VEN_INT&DEV_33D5" /f
-::reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "6" /f
-
-:: Intel(R) 300 Series Chipset Family SATA AHCI Controller
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "7" /t "REG_SZ" /d "PCI\VEN_8086&DEV_A353&SUBSYS_08191028&REV_10" /f
-::reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "7" /f
-
-:: Intel(R) Wireless Manageability
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "8" /t "REG_SZ" /d "SWC\842ADBFC-7499-407B-BD91-A52842585D0B" /f
-::reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "8" /f
-
-:: Realtek Audio Universal Service
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "9" /t "REG_SZ" /d "SWC\VEN_10EC&SID_0001" /f
-::reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "9" /f
-sc stop "RtkAudioUniversalService"
-sc config "RtkAudioUniversalService" start="disabled"
-
-:: STMicroelectronics 3-Axis Digital Accelerometer
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "10" /t "REG_SZ" /d "ACPI\VEN_SMO&DEV_8810" /f
-::reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs" /v "10" /f
+:: End
