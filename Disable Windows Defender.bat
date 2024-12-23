8dc1a9abcdbce5389fd730f5a1c316e36079c0f8
Sean Rhone <Espionage724@users.noreply.github.com>
2024-12-23 06:55:56 -0500

    Update Disable Windows Defender.bat

--- Disable Windows Defender.bat
+++ Disable Windows Defender.bat
@@ -1,1 +1,5 @@
-reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t "REG_DWORD" /d "1" /f
+NET stop windefend
+SC config windefend start=disabled
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t "REG_DWORD" /d "1" /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Defender\Features" /v "TamperProtection" /t "REG_DWORD" /d "0" /f
+sc qc "windefend"
