3fda24a51763416a525c2ae3a2c0937061e581d8
Sean Rhone <Espionage724@users.noreply.github.com>
2025-01-08 04:27:37 -0500

    Update pool.ntp.org Time Servers.bat

--- pool.ntp.org Time Servers.bat
+++ pool.ntp.org Time Servers.bat
@@ -1,1 +1,3 @@
-w32tm /config /syncfromflags:manual /manualpeerlist:"0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org"
+w32tm /config /syncfromflags:"MANUAL" /manualpeerlist:"0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org" /update
+w32tm /resync
+pause
