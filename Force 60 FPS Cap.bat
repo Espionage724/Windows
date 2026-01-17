f88cbe3dd00e37ec686eafedf79664250452e3ae
Sean Rhone <Espionage724@users.noreply.github.com>
2026-01-17 01:36:50 -0500

    DDraw/DX FPS cap

---
+++ Force 60 FPS Cap.bat
@@ -0,0 +1,4 @@
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\DirectDraw" /v "ForceRefreshRate" /t "REG_DWORD" /d "60" /reg:64 /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\DirectDraw" /v "ForceRefreshRate" /t "REG_DWORD" /d "60" /reg:32 /f
+
+:: End
