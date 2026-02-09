32d52f36ead2b04bc148d7555fa6b6c96a3f83cf
Sean Rhone <Espionage724@users.noreply.github.com>
2026-02-09 08:20:18 -0500

    Update Enable Native NVMe.bat

--- Enable Native NVMe.bat
+++ Enable Native NVMe.bat
@@ -1,8 +1,4 @@
-@echo off
-
-REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Policies\Microsoft\FeatureManagement\Overrides" /v "1176759950" /t "REG_DWORD" /d "1" /f
-REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Policies\Microsoft\FeatureManagement\Overrides" /v "156965516" /t "REG_DWORD" /d "1" /f
-REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Policies\Microsoft\FeatureManagement\Overrides" /v "1853569164" /t "REG_DWORD" /d "1" /f
-REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Policies\Microsoft\FeatureManagement\Overrides" /v "735209102" /t "REG_DWORD" /d "1" /f
-
-:: End
+"%SystemRoot%\System32\reg.exe" ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Policies\Microsoft\FeatureManagement\Overrides" /v "1176759950" /t "REG_DWORD" /d "1" /f
+"%SystemRoot%\System32\reg.exe" ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Policies\Microsoft\FeatureManagement\Overrides" /v "156965516" /t "REG_DWORD" /d "1" /f
+"%SystemRoot%\System32\reg.exe" ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Policies\Microsoft\FeatureManagement\Overrides" /v "1853569164" /t "REG_DWORD" /d "1" /f
+"%SystemRoot%\System32\reg.exe" ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Policies\Microsoft\FeatureManagement\Overrides" /v "735209102" /t "REG_DWORD" /d "1" /f
