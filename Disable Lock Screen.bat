1587b349add9de4a46b1ff9a43f927db71413fc9
Sean Rhone <Espionage724@users.noreply.github.com>
2026-02-09 08:14:39 -0500

    Update Disable Lock Screen.bat

--- Disable Lock Screen.bat
+++ Disable Lock Screen.bat
@@ -1,1 +1,1 @@
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v "NoLockScreen" /t REG_DWORD /d "1" /f
+"%SystemRoot%\System32\reg.exe" ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v "NoLockScreen" /t REG_DWORD /d "1" /f
