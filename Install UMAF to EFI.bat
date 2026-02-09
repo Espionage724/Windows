c33ae4ebbbc1a29bc1d35d7e4d173327007ee86c
Sean Rhone <Espionage724@users.noreply.github.com>
2026-02-09 08:00:55 -0500

    Update Install UMAF to EFI.bat

--- Install UMAF to EFI.bat
+++ Install UMAF to EFI.bat
@@ -1,16 +1,20 @@
-DEL /Q "%UserProfile%\Downloads\UMAF_BETA.zip" & "curl.exe" --output "%UserProfile%\Downloads\UMAF_BETA.zip" --location "https://github.com/DavidS95/Smokeless_UMAF/raw/refs/heads/main/UMAF_BETA.zip"
-RMDIR /S /Q "%UserProfile%\Downloads\UMAF_BETA" & MKDIR "%UserProfile%\Downloads\UMAF_BETA"
-"tar.exe" xf "%UserProfile%\Downloads\UMAF_BETA.zip" -C "%UserProfile%\Downloads\UMAF_BETA"
+@echo off
+
+DEL /Q "%UserProfile%\Downloads\UMAF_BETA.zip"
+"%SystemRoot%\System32\curl.exe" --output "%UserProfile%\Downloads\UMAF_BETA.zip" --location "https://github.com/DavidS95/Smokeless_UMAF/raw/refs/heads/main/UMAF_BETA.zip"
+RMDIR /S /Q "%UserProfile%\Downloads\UMAF_BETA"
+MKDIR "%UserProfile%\Downloads\UMAF_BETA"
+"%SystemRoot%\System32\tar.exe" xf "%UserProfile%\Downloads\UMAF_BETA.zip" -C "%UserProfile%\Downloads\UMAF_BETA"
 
 MOVE /Y "%UserProfile%\Downloads\UMAF_BETA\EFI\Boot\BOOTX64.efi" "%UserProfile%\Downloads\UMAF_BETA\EFI\Boot\umaf.efi"
 
-MOUNTVOL Y:\ /S
+"%SystemRoot%\System32\mountvol.exe" Y:\ /S
 
 MOVE /Y "%UserProfile%\Downloads\UMAF_BETA\*.efi" "Y:\"
 MOVE /Y "%UserProfile%\Downloads\UMAF_BETA\SREP_Config.cfg" "Y:\"
 MOVE /Y "%UserProfile%\Downloads\UMAF_BETA\EFI\Boot\umaf.efi" "Y:\EFI\Boot"
 
-MOUNTVOL Y:\ /D
+"%SystemRoot%\System32\mountvol.exe" Y:\ /D
 
 PAUSE
 
