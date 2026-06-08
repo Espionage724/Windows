ddb3435851e75b27006930c905065c247dcd9601
Sean Rhone <Espionage724@users.noreply.github.com>
2026-06-07 22:29:38 -0400

    Update Update Office.bat

--- Update Office.bat
+++ Update Office.bat
@@ -1,1 +1,7 @@
-"%CommonProgramFiles%\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update "user" forceappshutdown="True" displaylevel="True"
+@echo off
+
+CD "%Temp%"
+
+START "" /D "%Temp%" "%CommonProgramFiles%\microsoft shared\ClickToRun\OfficeC2RClient.exe" /update "user" forceappshutdown="True" displaylevel="True"
+
+:: End
