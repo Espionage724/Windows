a0ce97110b4e8a2fe75f4df60350799f1bd5a9f7
Sean Rhone <Espionage724@users.noreply.github.com>
2026-07-21 23:34:01 -0400

    Update Disable XHCI Interrupt Moderation.bat

--- Disable XHCI Interrupt Moderation.bat
+++ Disable XHCI Interrupt Moderation.bat
@@ -7,6 +7,6 @@
 ::"%ProgramFiles%\RW-Everything\Rw.exe" /Min /NoLogo /Stdout /Command="W32 0xC1222024 0x00000000"
 
 ::Auto-start
-::SCHTASKS /Create /SC "ONLOGON" /TN "XHCI IMOD" /TR "'%ProgramFiles%\RW-Everything\Rw.exe' /Min /NoLogo /Stdout /Command='W32 0xC1222024 0x00000000'" /F
+::"%SystemRoot%\System32\schtasks.exe" /Create /SC "ONLOGON" /TN "XHCI IMOD" /TR "'%ProgramFiles%\RW-Everything\Rw.exe' /Min /NoLogo /Stdout /Command='W32 0xC1222024 0x00000000'" /F
 
 :: End
