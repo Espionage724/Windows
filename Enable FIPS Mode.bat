f5b86007613b3164b4cf452c51737cdf68716a37
Sean Rhone <Espionage724@users.noreply.github.com>
2026-07-21 23:23:08 -0400

    Update Enable FIPS Mode.bat

--- Enable FIPS Mode.bat
+++ Enable FIPS Mode.bat
@@ -1,7 +1,1 @@
-REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa\FipsAlgorithmPolicy" /v "Enabled" /t "REG_DWORD" /d "1" /f
-
-"powershell.exe" -Command "$md5 = New-Object -TypeName 'System.Security.Cryptography.MD5CryptoServiceProvider'"
-
-PAUSE
-
-:: End
+"%SystemRoot%\System32\reg.exe" ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa\FipsAlgorithmPolicy" /v "Enabled" /t "REG_DWORD" /d "1" /f
